aa30a4e195ecb7195edb500e4646fa7a4674a005
Obioma <obeeomaa@gmail.com>
2023-12-11 21:24:41 +0100

    Add classes to color-code timeblocks
    Update index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,13 @@
 
 // * Display the current day at the top of the calender when a user opens the planner.
 $( document ).ready(function() {
- 
+
     // Display current day
     const currentDay = dayjs().format('dddd D, YYYY')
     $('#currentDay').text(currentDay);
 
     // * Present timeblocks for standard business hours when the user scrolls down.
+
     $('.time-block').hide();
     $(document).on('scroll', function () {  
    $( ".time-block" ).each(function() {
@@ -15,25 +16,67 @@ $( document ).ready(function() {
 });
 })
 
-
-
 // * Color-code each timeblock based on past, present, and future when the timeblock is viewed.
-    
+    const currentHour = dayjs().format('H'); // 0 - 23
+    console.log(currentHour)
+
+    //    console.log(first)
+    $('.time-block').each(function () {
+            // for each individual $('.time-block') element
+     
+            var indicatedHour = parseInt($(this).text())
+            console.log(indicatedHour)
+            // if elementHour < currentHour 
+            if(indicatedHour < currentHour)
+            //      this element is in the past
+            {
+
+                $(this).children().addClass('past')
+
+                }
+            // if elementHour == currentHour
+        if (indicatedHour == currentHour) {
+                
+            //      this element is now
+                 $(this).children().addClass('present')
+            }
+
+            // if elementHour > currentHour 
+        if (indicatedHour > currentHour) {
+                
+            //   this element is in the future  
+                $(this).children().addClass('future')
+              }
+
+        });
+ 
 
     
-// * Allow a user to enter an event when they click a timeblock
+    // * Allow a user to enter an event when they click a timeblock
     
+    
+    // * Save the event in local storage when the save button is clicked in that timeblock.
 
-    
-// * Save the event in local storage when the save button is clicked in that timeblock.    
-    
-    
+    $('.saveBtn').click(function () {
+var paren = $(this).parent(this)
+            console.log(paren)  
+        })
+
+    //     const eventDataObject = {
+    //         "day":$('#currentDay').text(),
+    //         "time": $('.time-block').text(),
+    //         "content":$('textarea').text()
+    //     }
+    //     console.log(eventDataObject)
+    // })
+})    
+    //document ready function 
+
     
     
 // * Persist events between refreshes of a page
     
     
-});
 
 
  
