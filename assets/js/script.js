17e56b8780caca83159a330a3699fa2b6fea9047
Obioma <obeeomaa@gmail.com>
2024-01-24 18:52:46 +0100

    Update variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,83 +1,62 @@
-// The app should:
+$(document).ready(function () {
+  // Formatted current day date at top calender
+  const currentDay = dayjs().format("dddd D, YYYY");
+  $("#currentDay").text(currentDay);
 
-// * Display the current day at the top of the calender when a user opens the planner.
-$( document ).ready(function() {
+  // Formatted current hour
+  const currentHour = dayjs().format("H"); // 0 - 23
 
-// Display current day
-    const currentDay = dayjs().format('dddd D, YYYY')
-    $('#currentDay').text(currentDay);
+  // For each individual timeblock element
+  $(".time-block").each(function () {
+    // Get text content of timeblock element
+    var timeblockHour = parseInt($(this).text());
 
-// * Present timeblocks for standard business hours when the user scrolls down.
+    //Color-code each timeblock based on past, present, and future
 
-// * Color-code each timeblock based on past, present, and future when the timeblock is viewed.
-    const currentHour = dayjs().format('H'); // 0 - 23
+    // If timeblockHour less than currentHour
+    if (timeblockHour < currentHour) {
+      // this element is in the past
+      $(this).children().eq(1).addClass("past");
+    }
+    // If timeblockHour equal to currentHour
+    if (timeblockHour == currentHour) {
+      // this element is now
+      $(this).children().eq(1).addClass("present");
+    }
 
-// Get reference to textarea
-    var textEl = $(this).children().eq(1)
+    // If timeblockHour greater than currentHour
+    if (timeblockHour > currentHour) {
+      // this element is in the future
+      $(this).children().eq(1).addClass("future");
+    }
+  });
 
-    $('.time-block').each(function () {
-            // for each individual $('.time-block') element
-     
-            var indicatedHour = parseInt($(this).text())
+  $(this).children().eq(1).addClass("description");
 
-            // if elementHour < currentHour 
-            if(indicatedHour < currentHour)
-            // this element is in the past
-            {
+  // * Save the event in local storage when the save button is clicked in that timeblock.
 
-               $(this).children().eq(1).addClass('past')
+  $("button").on("click", function (event) {
+    event.preventDefault();
+    var eventEntry;
+    var temp = $(this).prev();
+    // if textarea is empty
+    if (temp.val() === "") {
+      return;
+    } else {
+      // if textarea is not empty
+      eventEntry = temp.val();
+    }
 
-                }
-            // if elementHour == currentHour
-        if (indicatedHour == currentHour) {
-                
-            // this element is now
+    var timeOfDay = temp.attr("data-hour");
 
-               $(this).children().eq(1).addClass('present')
-            }
+    const eventEntryObject = {
+      day: currentDay,
+      content: eventEntry,
+    };
 
-        // if elementHour > currentHour
-        
-        if (indicatedHour > currentHour) {
-                
-            // this element is in the future  
-                $(this).children().eq(1).addClass('future')
-              }
+    localStorage.setItem(timeOfDay, JSON.stringify(eventEntryObject));
 
-        });
-    
-    // * Allow a user to enter an event when they click a timeblock
-    
- $(this).children().eq(1).addClass('description')
-
-    // * Save the event in local storage when the save button is clicked in that timeblock.
-
-    $('button').on('click',function (event) {
-        event.preventDefault()
-        var eventEntry;
-        var temp = $(this).prev()
-        // if textarea is empty
-        if (temp.val() === "") {
-            return
-        } else {
-            // if textarea is not empty
-            eventEntry = temp.val()
-        }
-
-        var timeOfDay = temp.attr('data-hour')
- 
-        const eventEntryObject = {
-
-            "day": currentDay,
-            "content":eventEntry
-        }
-
-        localStorage.setItem(timeOfDay, JSON.stringify(eventEntryObject))
-
-
-        // * Persist events between refreshes of a page
-    })  
-
-    }) 
-    //document ready function 
-
+    // * Persist events between refreshes of a page
+  });
+});
+//document ready function
